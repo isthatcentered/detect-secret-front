9893d312b6af4d3345e1b285b1798ad8397335b7
Edouard Penin <e.peninb@gmail.com>
2019-04-24 08:41:56 +0200

    Todos

---
+++ src/features/a-user-can-authenticate.ts
@@ -0,0 +1,21 @@
+/*
+ * 🛑 Login
+ *    - Page redirects to cloud function
+ *    - Cloud function redirects to page with token
+ *    - Page stores token
+ * 🛑 Logout
+ *    - A logout button is available when logged in
+ *    - Token is cleared on logout click
+ *    - User is redirected to home\log in with git page
+ *    - A confirmation message is displayed
+ */
+
+test( `A user can log in`, () => {
+
+} )
+
+test( `A user can log out`, () => {
+
+} )
+
+export default undefined
---
+++ src/features/a-user-can-test-a-repository-for-secrets.ts
@@ -0,0 +1,9 @@
+/*
+ * 🛑 Redirect unless logged in
+ */
+test( `A user can select a repository to test`, () => {
+
+} )
+
+
+export default undefined
